27f3131862f215fba8ac2caaed49f1ef6ac64246
TheTaurunti <thetaurunti@gmail.com>
2024-10-28 21:09:07 -0700

    2.0 update

--- settings.lua
+++ settings.lua
@@ -1,10 +1,18 @@
 data:extend({
-  {
-    type = "int-setting",
-    name = "Roboport64-logistic-area",
-    setting_type = "startup",
-    default_value = 64,
-    minimum_value = 10,
-    maximum_value = 1024
-  }
+	{
+		type = "int-setting",
+		name = "Roboport64-logistic-area",
+		setting_type = "startup",
+		default_value = 64,
+		minimum_value = 10,
+		maximum_value = 1024,
+		order = "1"
+	},
+	{
+		type = "bool-setting",
+		name = "Roboport64-scale-radar",
+		setting_type = "startup",
+		default_value = true,
+		order = "2"
+	}
 })
